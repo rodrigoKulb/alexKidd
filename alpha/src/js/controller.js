bf70b53253b7578949b1789b86146be4dae2c742
Victor Inknov <56061950+victorinknov@users.noreply.github.com>
2021-10-31 17:37:42 -0300

    função pause adicionada

--- alpha/src/js/controller.js
+++ alpha/src/js/controller.js
@@ -11,43 +11,50 @@ let divSoco = document.getElementById('soco'),
     divDireita = document.getElementById('moveDireita')
 
 function detectaClick(id) {
-    if (id == 'soco') {
-        personagem.soco(cenario);
-        punchSound.volume(0.4);
-        if (personagem.superForca == 2) {
-            personagem.vaisuperForca();
-            personagem.forcaAndando = 0;
+    
+    if (menu == 0) {
+        if (id == 'soco') {
+            personagem.soco(cenario);
+            punchSound.volume(0.4);
+            if (personagem.superForca == 2) {
+                personagem.vaisuperForca();
+                personagem.forcaAndando = 0;
+            }
+            if ((menu == 1) && (flechaMenu <= 690) && (personagem.superForca == 1)) {
+                personagem.superForca = 2;
+            }
         }
-        if ((menu == 1) && (flechaMenu <= 690) && (personagem.superForca == 1)) {
-            personagem.superForca = 2;
-        }
-    }
 
-    if (id == 'pulo') {
-        personagem.segueRight = 0;
-        personagem.segueLeft = 0;
-        personagem.pular();
-        console.log('PULEEEEEI')
-        if ((menu == 1) && (flechaMenu <= 690) && (personagem.superForca == 1)) {
-            personagem.superForca = 2;
+        if (id == 'pulo') {
+            personagem.segueRight = 0;
+            personagem.segueLeft = 0;
+            personagem.pular();
+            if ((menu == 1) && (flechaMenu <= 690) && (personagem.superForca == 1)) {
+                personagem.superForca = 2;
+            }
         }
-    }
 
-    // movimentacao
-    if (id == 'moveEsquerda') {
-        personagem.andar('left')
-        i = i - 2;
-    }
-    if (id == 'moveDireita') {
-        personagem.andar('right')
-        i = i + 2;
-    }
-    if (id == 'moveCima') {
-        personagem.abaixar()
-        b = b - 2;
-    } if (id == 'moveBaixo') {
-        personagem.abaixar()
-        b = b + 2;
+        // movimentacao
+        if (id == 'moveEsquerda') {
+            personagem.andar('left')
+            i = i - 2;
+        }
+        if (id == 'moveDireita') {
+            personagem.andar('right')
+            i = i + 2;
+        }
+        if (id == 'moveCima') {
+            personagem.abaixar()
+            b = b - 2;
+        } if (id == 'moveBaixo') {
+            personagem.abaixar()
+            b = b + 2;
+        }
+    } if (id == 'pause') {
+        let valorSom = true
+        if (menu == 0) menu = 2
+        else menu = 0 & chamaSom(valorSom)
+
     } else {
         personagem.parado()
     }
